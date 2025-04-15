15c4b39c4dc0b8ecc6b94a43f3ba98828694b2cd
蓑羽 諒一朗 <ryoichiro.minowa@ad.keit.co.jp>
2025-04-16 02:33:35 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Canvas from "./components/Canvas";
 import Canvas2 from "./components/Canvas2";
 import Canvas3 from "./components/Canvas3";
